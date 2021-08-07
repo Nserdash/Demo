bb784bf1a0fed4422d6d05a4c63b6887d54d7d9e
NIKITA <serdashenko@mail>
2021-08-07 15:42:59 +1000

    changes

--- src/Controllers/Auth/AuthController.php
+++ src/Controllers/Auth/AuthController.php
@@ -11,7 +11,10 @@ class AuthController
     {           
         $query = new QueryBuilder;
         
-        $data = $query->select('users')->where('login','=',Request::one('login'))->where('password','=',md5(Request::one('password')))->get();
+        $data = $query->select('users')
+        ->where('login','=',Request::one('login'))
+        ->where('password','=',md5(Request::one('password')))
+        ->get();
 
         if($data[0]->password == md5(Request::one('password'))) {
             session_start();
